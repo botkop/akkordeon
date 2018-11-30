937f6d6a454af6092b2786596ccb83380ad5d916
Koen Dejonghe <koen.dejonghe@cegeka.com>
2018-11-30 19:34:32 +0100

    std ci

---
+++ src/main/scala/botkop/akkordeon/mkl/Stageable.scala
@@ -0,0 +1,33 @@
+package botkop.akkordeon.mkl
+
+import akka.actor.{ActorRef, ActorSystem}
+
+trait Stageable {
+  def stage(implicit system: ActorSystem): ActorRef
+  def name: String
+}
+
+object Stageable {
+
+  def stage(ss: List[Stageable])(implicit system: ActorSystem): List[ActorRef] =
+    ss.map(_.stage)
+
+  def connect(gates: List[ActorRef]): List[ActorRef] = {
+    (None +: gates.map(g => Some(g)) :+ None).sliding(3, 1).foreach {
+      case List(p, c, n) =>
+        c.get ! Wire(p, n)
+    }
+    gates
+  }
+
+  def connect(sentinels: List[ActorRef], gates: List[ActorRef]): Unit = {
+    connect(gates)
+    sentinels.foreach { s =>
+      s ! Wire(Some(gates.last), Some(gates.head))
+    }
+  }
+
+  def connect(gates: List[Stageable])(
+      implicit system: ActorSystem): List[ActorRef] = connect(stage(gates))
+
+}
